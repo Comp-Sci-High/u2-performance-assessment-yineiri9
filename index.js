24a74f7d18caa51c0e92f7a4ad31f9108000ea7c
yineiri9 <yineiri.reyescepeda25@compscihigh.org>
2024-11-13 15:48:16 +0000

    idk

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+async function giveInformation(nba){
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'd1f6d42591msh9f6ab44a01815c7p11ee8cjsn7a3203b23676',
+            'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+
+        
+        }
+    };    
+}
+
+const url = 'https://api-nba-v1.p.rapidapi.com/games?date=2022-02-12';
+const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 
